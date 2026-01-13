73ee3d9f2fb6f620568f99180a6962cc1bb5c16e
trishitanalla <trishitanalla03@gmail.com>
2026-01-13 13:11:49 +0530

    fix: disable static export for vercel

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",          // 🔥 REQUIRED for static files
+  // output: "export",          // 🔥 REQUIRED for static files
+  // images: {
+  //   unoptimized: true,       // 🔥 REQUIRED for static export
+  //   remotePatterns: [
+  //     {
+  //       protocol: "https",
+  //       hostname: "images.unsplash.com",
+  //     },
+  //   ],
+  // },
   images: {
-    unoptimized: true,       // 🔥 REQUIRED for static export
     remotePatterns: [
       {
         protocol: "https",
